ccc8d5a7873dc62abfa7700be9529af29e101597
Olexandr Buzhenko <OBuzhenko@luxoft.com>
2020-02-25 13:21:56 +0200

    Add arrow

--- Assets/Code/Gun/Bow_bullet.cs
+++ Assets/Code/Gun/Bow_bullet.cs
@@ -2,14 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bow_bullet : MonoBehaviour, Bullet
+public class Arrow : MonoBehaviour, Bullet
 {
     public float hit { get; set; }
     public float radius { get; set; }
     public bool is_collision { get; set; }
 
-    public Bow_bullet()
+    public int Speed;
+    Vector3 lastPos;
+    public GameObject decal;
+
+    void Start()
+    {
+        lastPos = transform.position;
+    }
+
+    void Update()
+    {
+      //  transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+
+
+    }
+
+     public Arrow()
     {
 
     }
+
+    public void SetToRope(Transform ropeTransform)
+    {
+        transform.parent = ropeTransform;
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+
+        Rigidbody.isKinematic = true;
+    }
+
+    public void Shot(float velocity)
+    {
+        transform.parent = null;
+        Rigidbody.isKinematic = false;
+        Rigidbody.velocity = transform.forward * velocity;
+    }
+
+   
 }
